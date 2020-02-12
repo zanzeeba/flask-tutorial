b76e3379c999ad736f9e8be8ffe8f463c559d4c7
johnmontaguehowitt <jmhowitt@mac.com>
2020-02-12 13:20:47 +0000

    all working and tests working

--- rotasort/__init__.py
+++ rotasort/__init__.py
@@ -28,7 +28,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello/')
+    @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
